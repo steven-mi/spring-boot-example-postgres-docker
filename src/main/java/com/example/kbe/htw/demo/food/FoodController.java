e0e120b7fe8390369b0d1451e8a500d431a0e1a8
Steven Mi <steven.mi@getyourguide.com>
2022-11-14 23:46:27 +0100

    chore: remove unused exception handler

--- src/main/java/com/example/kbe/htw/demo/food/FoodController.java
+++ src/main/java/com/example/kbe/htw/demo/food/FoodController.java
@@ -19,19 +19,6 @@ import java.util.Optional;
 @RequestMapping("/food")
 public class FoodController {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationExceptions(
-            MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return errors;
-    }
-
     @Autowired
     FoodRepository foodRepository;
 
