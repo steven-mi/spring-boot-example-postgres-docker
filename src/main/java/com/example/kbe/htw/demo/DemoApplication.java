0266844d8b588806c256a55212f6cf4e7ec12947
Steven Mi <steven.mi@getyourguide.com>
2022-11-14 23:36:17 +0100

    refactor: rename product to food

--- src/main/java/com/example/kbe/htw/demo/DemoApplication.java
+++ src/main/java/com/example/kbe/htw/demo/DemoApplication.java
@@ -1,19 +1,11 @@
 package com.example.kbe.htw.demo;
 
-import com.example.kbe.htw.demo.product.Product;
-import com.example.kbe.htw.demo.product.ProductRepository;
+import com.example.kbe.htw.demo.food.Food;
+import com.example.kbe.htw.demo.food.FoodRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -23,12 +15,12 @@ public class DemoApplication {
     }
 
     @Bean
-    public CommandLineRunner demo(ProductRepository repository) {
+    public CommandLineRunner demo(FoodRepository repository) {
         return (args) -> {
             // save a few customers
-            repository.save(new Product(1L, "Chicken Boneless (ohne Knochen)", "1) Plain no sauce 2) Sweet chili (Peanut topping) 3) Soy garlic 4) Super spicy 5) Soy wasabi", 12.50F, "EUR", "food"));
-            repository.save(new Product(2L, "Chicken Cuts (mit Knochen)", "1) Plain no sauce 2) Sweet chili (Peanut topping) 3) Soy garlic 4) Super spicy 5) Soy wasabi", 12.00F, "EUR", "food"));
-            repository.save(new Product(3L, "Beef Dumpling", "BBQ Beef Fried Dumpling (4 PCS)", 6.50F, "EUR", "side"));
+            repository.save(new Food(1L, "Chicken Boneless (ohne Knochen)", "1) Plain no sauce 2) Sweet chili (Peanut topping) 3) Soy garlic 4) Super spicy 5) Soy wasabi", 12.50F, "EUR", "food"));
+            repository.save(new Food(2L, "Chicken Cuts (mit Knochen)", "1) Plain no sauce 2) Sweet chili (Peanut topping) 3) Soy garlic 4) Super spicy 5) Soy wasabi", 12.00F, "EUR", "food"));
+            repository.save(new Food(3L, "Beef Dumpling", "BBQ Beef Fried Dumpling (4 PCS)", 6.50F, "EUR", "side"));
         };
 
     }
--- src/main/java/com/example/kbe/htw/demo/food/FoodRepository.java
+++ src/main/java/com/example/kbe/htw/demo/food/FoodRepository.java
@@ -1,10 +1,10 @@
-package com.example.kbe.htw.demo.product;
+package com.example.kbe.htw.demo.food;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface FoodRepository extends JpaRepository<Food, Long> {
 
 
 }
